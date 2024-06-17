d8747dae3a361b25a5793401e71bc290fbeb0b6c
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-06-17 14:31:13 -0500

    feat(routes): agregar rutas de login, logout y user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['api', 'auth:sanctum'], 'prefix' => 'v1'], function () {
+    
+    Route::get('/user', [UserController::class, 'index']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
+Route::group(['middleware' => 'api', 'prefix' => 'v1'], function () {
+    Route::post('/login', [AuthController::class, 'login']);
 });
