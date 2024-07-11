d98eb84b2ca688b0b05d7ceb5ead68dba5657be9
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-07-10 22:28:54 -0500

    feat(pothole): agregar acceso a URL de la imagen en el modelo Pothole
    - Se ha añadido el método `image` al modelo `Pothole`.
      - Este método devuelve un atributo `Attribute` que transforma el valor almacenado en la base de datos en una URL accesible públicamente utilizando el disco `public` de Laravel.
    
    Este cambio permite acceder a la URL completa de la imagen asociada a un bache, mejorando la forma en que las imágenes se manejan y se muestran en la aplicación.

--- app/Models/Pothole.php
+++ app/Models/Pothole.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Pothole extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function image(): Attribute
+    {
+        return new Attribute(get: fn ($attr) => $attr ? Storage::disk('public')->url($attr) : null);
+    }
 }
