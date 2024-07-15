7d18a7b01246cd292a5af56f6e4433ef6671dc93
Cristhian Moreno <cristian_21_97@hotmail.com>
2024-07-15 22:51:23 +0000

    chore(docker): actualizar archivos para produccion

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
-   set -e
+set -e
 
-   composer update
-   php /var/www/html/artisan db:wipe
-   php /var/www/html/artisan migrate:refresh --seed
-   php /var/www/html/artisan key:generate
-   rm -rf /var/www/html/public/storage
-   php /var/www/html/artisan storage:link
+# Ajusta permisos de las carpetas necesarias
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
-   supervisord -c /etc/supervisor/laravel-worker.conf
-   php-fpm
+echo "Waiting 15 seconds for MySQL..."
+sleep 15
+
+# Instala dependencias de Node.js y construye los assets
+npm install
+npm run build
+
+composer update
+php /var/www/html/artisan db:wipe
+php /var/www/html/artisan migrate:refresh --seed
+php /var/www/html/artisan key:generate
+rm -rf /var/www/html/public/storage
+php /var/www/html/artisan storage:link
+
+echo "Laravel config success"
+
+supervisord -c /etc/supervisor/laravel-worker.conf &
+php-fpm
